83e766ed4dc48cc0274d3e33ec94d1cda5aab81c
Gautham B A <gautham.bangalore@gmail.com>
2015-10-18 13:10:20 +0530

    Rebooted the project.
    This commit contains the UI for the parser and the corresponding output
    table.

---
+++ src/InputGrammar.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+/**
+ * Created by gauth_000 on 13-Oct-15.
+ */
+public class InputGrammar extends JFrame implements ActionListener
+{
+    public ArrayList<String> getProductions()
+    {
+        return productions;
+    }
+
+    private ArrayList<String> productions;
+    private JTextArea inputGrammarArea;
+
+    public InputGrammar()
+    {
+        productions=new ArrayList<String>();
+
+        setSize(250, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel pane=new JPanel();
+        inputGrammarArea =new JTextArea(10, 20);
+        JScrollPane inputScrollPane=new JScrollPane(inputGrammarArea);
+        JButton okButton=new JButton("Ok");
+
+        okButton.addActionListener(this);
+
+        pane.add(inputScrollPane);
+        pane.add(okButton);
+
+        setContentPane(pane);
+        setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        new InputGrammar();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        for(String production:inputGrammarArea.getText().split("\n"))
+        {
+            System.out.println(production);
+            productions.add(production);
+        }
+    }
+}
