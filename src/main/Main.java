ad2184a8f6e55aeb549da5a755f44201cf87ec81
Adithya Abraham Philip <adithyaphilip@gmail.com>
2015-10-31 11:32:26 +0530

    updated grammar

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,10 +10,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<String> l = new ArrayList<>();
-		l.add("A>B G |C|  D|E F .");
+		l.add("A>BG |C|  D|E F .");
 		l.add("B>C");
 		l.add("B>E");
 		Grammar g = InputParser.parseInput(l);
-		System.out.println(g);
+		System.out.println(g.nonTerminals);
 	}
 }
+	
